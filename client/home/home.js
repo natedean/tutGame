d38c2a0492ffbaddd9665adfb19030fc15280fb8
Nate <natedeanmail@gmail.com>
2014-08-22 14:25:10 -0500

    Started on the basic layout for a tutorial game.  Gameboard styled.  Dynamic slide templates set up

--- client/home/home.js
+++ client/home/home.js
@@ -1,20 +1,28 @@
-var clickCounter = 0;
+Session.setDefault('readyForNext', true);
+Session.setDefault('slideNumber', 1);
+Session.setDefault('playerScore', 0);
 
-  Template.userArea.greeting = function () {
-    return "You logged in!  Give yourself a pat on the back.";
-  };
+Template.home.helper = function(){
+  return "slide" + Session.get('slideNumber');
+}
 
-  Template.userArea.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-    clickCounter++;  
-    var p = $('#greeting'); 
-     if(clickCounter === 1){
-       p.html('Hello!');
-     }else{
-       p.html('You have pushed the green button ' + clickCounter + ' times.<br>  Are you bored yet?');    
-     }
-    
-    }  
-  });
+Template.home.showNextBtn = function(){
+  return Session.get('readyForNext');
+}
+
+Template.home.events({
+  'click #nextSlide': function(){
+    var nextSlide = Session.get('slideNumber') + 1;
+    Session.set('slideNumber', nextSlide);
+    Session.set('readyForNext',false);
+  },
+  'click #slideCompleted': function(){
+    Session.set('readyForNext',true);
+  }
+});
+
+Template.home.playerScore = function(){
+  return Session.get('playerScore');
+} 
+
 
